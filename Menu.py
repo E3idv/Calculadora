4e32c98d3374c1b1251e4f267dd85cd1052aeef0
idv17cordoba <31976387+idv17cordoba@users.noreply.github.com>
2017-09-28 15:23:10 -0500

    Add files via upload

---
+++ Menu.py
@@ -0,0 +1,31 @@
+def Menu():
+    ans = True
+    while ans:
+        print("""
+        1.-Suma
+        2.-Multiplicaccion
+        3.-Division
+        4.-Modullo
+        5.-Potencia
+        6.-Raiz
+        7.-Binario-Hexadecimal
+        8.-Hexadecimal-Binario
+        9.-Decimal-Hexadecimal
+        10.-Hexadecimal-Decimal
+        11.-Deciaml-Binario
+        12.-Binario-Decimal
+        13.-Numeros primos en un rango definido
+        14.-Determinar si un numero es primo
+        15.-IMC
+        16.-Salir y acabar de sufrir
+        """)
+
+        ans=int(input("Que te gustaria hacer"))
+       
+
+
+Menu()
+
+
+
+
